12dd712dc988e67344e2bcf5b9749dde05cdadb5
Sofia Bargues <barguessofia@gmail.com>
2024-08-19 19:31:59 +0100

    filteredWords.map((cosa)

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,37 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const words = ["okaxis", "BARODAMPAY", "rbl", "upi", "allbank"];
+
 function App() {
+  const [word, setWord] = useState("");
+  function onChange(e) {
+    setWord(e.target.value);
+    console.log(word);
+  }
+
+  const filteredWords = words.filter((word) => word.length);
+
   return (
-    <div className="flex-col flex border-[#66c87b] w-96 rounded-lg p-9 border-2 gap-5 m-auto ">
+    <form className="flex-col flex border-[#66c87b] w-96 rounded-lg p-9 border-2 gap-5 m-auto ">
       <div>
         <img src="/autocomplete.png" />
       </div>
-      <input placeholder="Enter your UPI id "></input>
+      <input
+        onChange={onChange}
+        className="inline-block"
+        placeholder="Enter your UPI id "
+      ></input>
+
+      {}
       <hr />
       <button className=" bg-[#66c87b] font-semibold text-white p-2 w-full">
-        Play Now
+        Pay Now
       </button>
-    </div>
+      {filteredWords.map((cosa) => (
+        <li>{cosa}</li>
+      ))}
+    </form>
   );
 }
 
