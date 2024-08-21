775907f21066404944516ce6b0d722bbbde05455
Sofia Bargues <barguessofia@gmail.com>
2024-08-21 10:01:22 +0100

    show the correct word drower

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,12 @@ function App() {
   const [word, setWord] = useState("");
   function onChange(e) {
     setWord(e.target.value);
-    console.log(word);
   }
 
   const filteredWords = words.filter((string) => string.startsWith(word));
 
   return (
-    <form className="flex-col flex border-[#66c87b] w-96 rounded-lg p-9 border-2 gap-5 m-auto ">
+    <form className="flex-col flex border-[#66c87b] w-96 rounded-lg p-9 border-2 m-auto ">
       <div>
         <img src="/autocomplete.png" />
       </div>
@@ -24,15 +23,19 @@ function App() {
         className="inline-block"
         placeholder="Enter your UPI id "
       ></input>
-
-      {}
       <hr />
+
+      <div className={word && filteredWords ? "" : "hidden"}>
+        {filteredWords.map((cosa) => (
+          <div className="border p-2">
+            <h3>{cosa}</h3>
+          </div>
+        ))}
+      </div>
+
       <button className=" bg-[#66c87b] font-semibold text-white p-2 w-full">
         Pay Now
       </button>
-      {filteredWords.map((cosa) => (
-        <li>{cosa}</li>
-      ))}
     </form>
   );
 }
