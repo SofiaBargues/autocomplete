632782366382951a89decb9d9af93486edd55ed5
Sofia Bargues <barguessofia@gmail.com>
2024-08-21 10:37:21 +0100

    select a suggestion and see it on the input

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,23 @@ const words = ["okaxis", "BARODAMPAY", "rbl", "upi", "allbank"];
 
 function App() {
   const [word, setWord] = useState("");
+  // const [value, setValue] = useState("");
   function onChange(e) {
     setWord(e.target.value);
   }
 
   const filteredWords = words.filter((string) => string.startsWith(word));
+  {
+    console.log(word);
+  }
 
   return (
-    <form className="flex-col flex border-[#66c87b] w-96 rounded-lg p-9 border-2 m-auto ">
+    <form className="flex-col flex relative border-[#66c87b] w-96 rounded-lg p-9 border-2 m-auto ">
       <div>
         <img src="/autocomplete.png" />
       </div>
       <input
+        value={word}
         onChange={onChange}
         className="inline-block"
         placeholder="Enter your UPI id "
@@ -27,13 +32,20 @@ function App() {
 
       <div className={word && filteredWords ? "" : "hidden"}>
         {filteredWords.map((cosa) => (
-          <div className="border p-2">
-            <h3>{cosa}</h3>
+          <div className="border p-2 bg-white w-[308px] absolute">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setWord(cosa);
+              }}
+            >
+              {cosa}
+            </button>
           </div>
         ))}
       </div>
 
-      <button className=" bg-[#66c87b] font-semibold text-white p-2 w-full">
+      <button className=" bg-[#66c87b] font-semibold  text-white p-2 w-full">
         Pay Now
       </button>
     </form>
